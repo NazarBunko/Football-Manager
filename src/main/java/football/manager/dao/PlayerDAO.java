54732acbbea1de8fb1701aa1cc1df2786136ae6f
NazarBunko <bunkonazar@gmail.com>
2025-01-02 17:16:42 +0200

    added base functional in dao

--- src/main/java/football/manager/dao/PlayerDAO.java
+++ src/main/java/football/manager/dao/PlayerDAO.java
@@ -1,8 +1,62 @@
 package football.manager.dao;
 
+import football.manager.model.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class PlayerDAO {
+    static JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    public PlayerDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public PlayerDAO() {}
+
+    public List<Player> index() {
+        return jdbcTemplate.query("SELECT * FROM player", new BeanPropertyRowMapper<>(Player.class));
+    }
+
+    public Long add(Player player) {
+        Integer maxId = jdbcTemplate.queryForObject("SELECT COALESCE(MAX(id), 0) FROM player", Integer.class);
+
+        Long newId = (long) ((maxId != null ? maxId : 0) + 1);
+        player.setId(newId);
+
+        jdbcTemplate.update("INSERT INTO users (id, team_id, name, age, position, month_experience) VALUES (?, ?, ?, ?, ?, ?)",
+                player.getId(),
+                player.getTeam_id(),
+                player.getName(),
+                player.getAge(),
+                player.getPosition(),
+                player.getMonthExperience()
+        );
+
+        return player.getId();
+    }
+
+    public Player show(Long id) {
+        return jdbcTemplate.queryForObject("SELECT * FROM player WHERE id = ?", new BeanPropertyRowMapper<>(Player.class), id);
+    }
+
+    public void delete(Long id) {
+        jdbcTemplate.update("DELETE FROM player WHERE id = ?", id);
+    }
+
+    public void update(Long id, Player updatedPlayer) {
+        jdbcTemplate.update("UPDATE users SET team_id = ?, name = ?, age = ?, position = ?, month_experience = ? WHERE id = ?",
+                updatedPlayer.getTeam_id(),
+                updatedPlayer.getName(),
+                updatedPlayer.getAge(),
+                updatedPlayer.getPosition(),
+                updatedPlayer.getMonthExperience(),
+                id
+        );
+    }
 }
--- src/main/java/football/manager/dao/TeamDAO.java
+++ src/main/java/football/manager/dao/TeamDAO.java
@@ -1,8 +1,60 @@
 package football.manager.dao;
 
+import football.manager.model.Team;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class TeamDAO {
 
+    private static JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public TeamDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public TeamDAO() {}
+
+    public List<Team> index() {
+        return jdbcTemplate.query("SELECT * FROM team", new BeanPropertyRowMapper<>(Team.class));
+    }
+
+    public Long add(Team team) {
+        Integer maxId = jdbcTemplate.queryForObject("SELECT COALESCE(MAX(id), 0) FROM team", Integer.class);
+
+        Long newId = (long) ((maxId != null ? maxId : 0) + 1);
+        team.setId(newId);
+
+        jdbcTemplate.update("INSERT INTO team (id, name, money, percent) VALUES (?, ?, ?, ?)",
+                team.getId(),
+                team.getName(),
+                team.getMoney(),
+                team.getPercent()
+        );
+
+        return team.getId();
+    }
+
+    public Team show(Long id) {
+        return jdbcTemplate.queryForObject("SELECT * FROM team WHERE id = ?", new BeanPropertyRowMapper<>(Team.class), id);
+    }
+
+    public void delete(Long id) {
+        jdbcTemplate.update("DELETE FROM team WHERE id = ?", id);
+    }
+
+    public void update(Long id, Team updatedTeam) {
+        jdbcTemplate.update("UPDATE team SET name = ?, money = ?, percent = ? WHERE id = ?",
+                updatedTeam.getName(),
+                updatedTeam.getMoney(),
+                updatedTeam.getPercent(),
+                id
+        );
+    }
 }
+
